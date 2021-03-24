8331c9a8f043a710c3f7fd84d44da53ed6dac6d3
Alexander Van Nevel <AiVa@ORD09393.local>
2021-03-24 12:07:03 +0100

    all parameters obtained

--- ai/CSV processing/CSV_processing.sh
+++ ai/CSV processing/CSV_processing.sh
@@ -2,6 +2,7 @@
 
 filename="../dump.csv"
 res=""
+
 if grep -i -q "<html>" "$filename"; then
 	res+="y "
 else
@@ -67,8 +68,65 @@ emails=$(cut -f5 -d ',' $filename | grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+.
 
 urls=$(cut -f4 -d ',' $filename | egrep -o 'https?://[^ ]+')
 
-echo $emails
-echo $urls
+while IFS= read -r var; do
+	i=0
+	reply=$(grep -o . <<<$var)
+	while IFS= read -r ch; do
+		[ $ch = "." ] && ((i++))
+	done <<< "$reply"
+	if [ "$i" -gt 2 ];then
+		break
+	fi
+done <<< "$emails"
 
+if [ "$i" -gt 2 ]; then
+	res+="y "
+else
+	res+="n "
+fi
+
+if grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" <<<"$urls"; then
+	res+="y "
+else
+	res+="n "
+fi
+
+email=(${emails[@]})
+match="n "
+while IFS= read -r sender; do
+	email=$(tr '[:upper:]' '[:lower:]' <<<"$email")
+	sender=$(tr '[:upper:]' '[:lower:]' <<<"$sender")
+	if [ "$email" != "$sender" ]; then
+ 		match="y "
+		break
+	fi
+done <<< "$emails"
+res+=$match
+
+domains=$(awk -F\/ '{ a[$1"//"$3]++ } END {for (i in a) print a[i], i }' <<<"$urls")
+
+#std email from Ordina contains 5 URLs
+i=-5
+while IFS= read -r domain; do
+	((i++))
+done<<< "$domains"
+
+if [ "$i" -gt 0 ]; then
+	res+="y "
+	echo $domains
+else
+	res+="n "
+fi
+
+#At least 5 HEXA chars required
+match="n "
+while IFS= read -r url; do
+	if [[ "$url" =~ .*\/[a-fA-F0-9]{5,}\/.* ]]; then
+		match="y "
+		echo $url
+		break
+	fi
+done<<< "$urls"
+res+=$match
 
 echo $res
